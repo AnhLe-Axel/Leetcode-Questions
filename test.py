c59b456b25c559a5e91fb250ea8b676ab90ed97a
Quang Anh <lqa.uom.cs@gmail.com>
2025-01-08 18:10:30 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,33 @@
 from typing import List
+import copy
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max = 0
-        if len(s) == 0:
-            return 0
-        for i in range(len(s)-1):
-            count = 1
-            for j in range(i + 1, len(s)):
-                if s[j] in s[i:j]:
-                    if count > max: 
-                        max = count
-                        break
+    def gameOfLife(self, board: List[List[int]]) -> None:
+
+        board_copy = copy.deepcopy(board)
+        rows, cols = len(board), len(board[0])
+        for i in range(rows):
+            for j in range(cols):
+                live_neighbors = self.count_live_neighbors(board_copy, i, j)
+                if board_copy[i][j] == 1:
+                    if live_neighbors < 2 or live_neighbors > 3:
+                        board[i][j] = 0
+                    else:
+                        board[i][j] = 1
                 else:
-                    count += 1
-        return max
+                    if live_neighbors == 3:
+                        board[i][j] = 1
+                    else:
+                        board[i][j] = 0
+
+    def count_live_neighbors(self, board: List[List[int]], i, j) -> None:
+        live_neighbors = 0
+        rows, cols = len(board), len(board[0])
+        for x in range(max(0, i-1), min(rows, i+2)):
+            for y in range(max(0, j-1), min(cols, j+2)):
+                if x != i or y != j:
+                    live_neighbors += board[x][y]
+        return live_neighbors
 
 sol = Solution()
-print(sol.lengthOfLongestSubstring("pwwkew"))
+print(sol.gameOfLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]))
